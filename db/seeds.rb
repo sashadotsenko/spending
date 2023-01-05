9363bb6dcdfac56816daa2d59bb4e87aa188f104
oleksandrdotsenko <oleksandr.dotsenko@evowill.com>
2023-01-05 17:06:03 +0200

    added data to seed file and placeholder to amount filter form.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,31 @@ sample_categories = [
 ].freeze
 
 Category.create!(sample_categories)
+
+
+(1..5).each do |i|
+  user = User.create!(
+          login: Faker::Name.first_name,
+          password: '123456'
+  )
+  (1..5).each do |c|
+    user.costs.create!(
+      description: 'Some description' ,
+      amount: Faker::Commerce.price,
+      category_id: Category.find(c).id
+    )
+  end
+end
+
+test_user = User.create!(
+              login: 'test_user',
+              password: '123456'
+)
+
+(1..5).each do |c|
+  test_user.costs.create!(
+    description: 'Some description' ,
+    amount: Faker::Commerce.price,
+    category_id: Category.find(c).id
+  )
+end
